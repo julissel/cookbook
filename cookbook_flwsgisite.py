8951ae2b2994e84b8507b3fdddf89acbc8c51d05
Julia Selyutina <jusereda@gmail.com>
2020-05-23 01:16:35 +0300

    Added about.html and index.html templates.

--- cookbook_flwsgisite.py
+++ cookbook_flwsgisite.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/index")
 @app.route("/")
 def index():
-    return "index"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return "<h1> О сайте </h1>"
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
