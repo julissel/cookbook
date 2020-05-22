e25ce9e73d5b55594cb31a49a8686d0a25e4e14b
Julia Selyutina <jusereda@gmail.com>
2020-05-23 01:40:56 +0300

    Information in templates has been expanded.

--- cookbook_flwsgisite.py
+++ cookbook_flwsgisite.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ["Intro", "Dish of the day (recipe)", "Contact us"]
+
 @app.route("/index")
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", menuitems=menu)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About site", menuitems=menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
