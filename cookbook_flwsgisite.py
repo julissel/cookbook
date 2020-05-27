7292ded1b901abf38b2b3d104bca1c86fe14da46
Julia Selyutina <jusereda@gmail.com>
2020-05-27 20:58:53 +0300

    Added url_for function for testing common path and dynamic path.

--- cookbook_flwsgisite.py
+++ cookbook_flwsgisite.py
@@ -1,25 +1,31 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 menu = ["Intro", "Dish of the day (recipe)", "Contact us"]
+
 
 @app.route("/index")
 @app.route("/")
 def index():
-    print(url_for('index'))
     return render_template("index.html", menuitems=menu)
+
 
 @app.route("/about")
 def about():
-    print(url_for('about'))
     return render_template("about.html", title="About site", menuitems=menu)
+
+
+@app.route("/profile/<int:country>/<path:username>")
+def profile(username, country):
+    return f"User: {username}, country {country}"
 
 
 # context manager
 #with app.test_request_context():
 #    print('url for about-page =', url_for('about'))
 #    print('url for index-page =', url_for('index'))
+#    print('url for profile =', url_for("profile", username='Maria', country=643))
 
 
 if __name__ == "__main__":
