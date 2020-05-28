53bcdd2450f03408dd3e7c12f6d42045e48e07ff
Julia Selyutina <jusereda@gmail.com>
2020-05-28 21:57:35 +0300

    Added styles.css and contacts page.

--- cookbook_flwsgisite.py
+++ cookbook_flwsgisite.py
@@ -1,24 +1,34 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 
 app = Flask(__name__)
-menu = ["Intro", "Dish of the day (recipe)", "Contact us"]
+menu = [{"name": "Intro", "url": "intro-cookbook"},
+        {"name": "Dish of the day (recipe)", "url": "main-dish"},
+        {"name": "Contact us", "url": "contact"}]
 
 
 @app.route("/index")
 @app.route("/")
 def index():
-    return render_template("index.html", menuitems=menu)
+    return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="About site", menuitems=menu)
+    return render_template("about.html", title="About site", menu=menu)
 
 
 @app.route("/profile/<int:country>/<path:username>")
 def profile(username, country):
     return f"User: {username}, country {country}"
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form['username'])
+
+    return render_template('contacts.html', title='Contact us', menu=menu)
 
 
 # context manager
